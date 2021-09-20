fc5f24a6becb1949ac5919e7897ac5ac60680cd3
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2021-09-20 12:08:57 +0800

    Add project files.

---
+++ VoiceInHandDev/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using VoiceInHandDev.Services;
+using VoiceInHandDev.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace VoiceInHandDev
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=f630f53e-86ec-4c48-853a-00d5de96201f;" +
+                  "ios=b0de3ea8-d031-48f6-97ec-503f424f2e0a",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ VoiceInHandDev/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using VoiceInHandDev.ViewModels;
+using VoiceInHandDev.Views;
+using Xamarin.Forms;
+
+namespace VoiceInHandDev
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
