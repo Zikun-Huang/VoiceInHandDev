78e4aec78d475a735bc7dce29796bdb6a9f74ad2
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2021-10-07 15:24:16 +0800

    Adding a few more things: - Add two sub pages which demonstrates sign to word or voice to sign. - Add video to media folder

--- VoiceInHandDev/Models/SavedItemConstants.cs
+++ VoiceInHandDev/Models/SavedItemConstants.cs
@@ -9,12 +9,12 @@ namespace VoiceInHandDev.Models
     {
         public static List<SavedItemModel> SavedItems = new List<SavedItemModel>
         {
-            new SavedItemModel(){SavedName="How are you?",SavedTime="2021-06-01-13:59"},
-            new SavedItemModel(){SavedName="I am good.",SavedTime="2021-05-29-13:59"},
-            new SavedItemModel(){SavedName="Thank you!",SavedTime="2021-05-28-13:59"},
-            new SavedItemModel(){SavedName="And you?",SavedTime="2021-05-05-13:59"},
-            new SavedItemModel(){SavedName="Me too.",SavedTime="2021-03-03-13:59"},
-            new SavedItemModel(){SavedName="Nice to meet you.",SavedTime="2021-02-08-13:59"}
+            new SavedItemModel(){SavedName="你就放心吧",SavedTime="2021-06-01-13:59"},
+            new SavedItemModel(){SavedName="没关系",SavedTime="2021-05-29-13:59"},
+            new SavedItemModel(){SavedName="没什么问题",SavedTime="2021-05-28-13:59"},
+            new SavedItemModel(){SavedName="我看过检查报告了",SavedTime="2021-05-05-13:59"},
+            new SavedItemModel(){SavedName="回去好好休息",SavedTime="2021-05-05-13:59"},
+            new SavedItemModel(){SavedName="下个月回来复查",SavedTime="2021-05-05-13:59"}
         };
     }
 }
